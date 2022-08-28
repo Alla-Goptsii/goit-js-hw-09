0f845505ea5da456edac74aa6a7a53f42779eadb
Alla-Goptsii <goptsy.a@gmail.com>
2022-08-28 18:14:04 +0300

    task-1 ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let intervalId = null;
 
+refs.btnStart.addEventListener('click', onClickBtnStart);
+function onClickBtnStart() {
+  let isActive = false;
+
+  if (isActive) {
+    return;
+  }
+  intervalId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  isActive = true;
+}
+
+refs.btnStop.addEventListener('click', onClickBtnStop);
+function onClickBtnStop() {
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
