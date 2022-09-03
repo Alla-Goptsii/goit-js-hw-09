84463663635846ccb44feeca00d64b5bc698df64
Alla-Goptsii <goptsy.a@gmail.com>
2022-09-03 21:41:32 +0300

    task-2 without show-brouser

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -10,24 +10,10 @@ const refs = {
   seconds: document.querySelector('[data-seconds]'),
   minutes: document.querySelector('[data-minutes]'),
 };
-console.log(refs.input);
 
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-    // const different = selectedDates[0] - dateNow;
-    // console.log('залишилось часу', different);
-    // console.log(options.);
-  },
-};
-
-const dateNow = Date.now();
-console.log('час зараз', dateNow);
 let selectedDate = null;
+refs.buttonStart.disabled = true;
+// refs.buttonStart = 'disabled';
 
 const startTime = flatpickr(refs.input, {
   enableTime: true,
@@ -35,48 +21,56 @@ const startTime = flatpickr(refs.input, {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] < dateNow) {
-      alert('Please choose a date in the future');
-      return;
+    if (selectedDates[0].getTime() < Date.now()) {
+      return Notiflix.Notify.failure('Please choose a date in the future');
     }
-    if (selectedDates[0] >= dateNow) {
+    if (selectedDates[0].getTime() > Date.now()) {
       selectedDate = selectedDates[0];
-      console.log('обрана дата', selectedDate.getTime());
+
+      refs.buttonStart.disabled = false;
       refs.buttonStart.addEventListener('click', onStartTime);
     }
-    const diffrentTime = selectedDate.getTime() - dateNow;
-    console.log('залишилося', diffrentTime);
   },
 });
 
-function onStartTime() {
-  console.log('buttonClick', 'click');
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
-    //   console.log({ 'days': ${days}, 'hours':${hours}, 'minutes':${minutes}, 'seconds':${seconds} });
-  }
-
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
 
-// addLeadingZero(value).padStart();
+function onStartTime() {
+  refs.buttonStart.disabled = true;
+
+  setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = selectedDate.getTime() - currentTime;
+    const { days, hours, minutes, seconds } = convertMs(deltaTime);
+    // const { time } = convertMs(deltaTime);
+
+    console.log(`${days}::${hours}::${minutes}::${seconds}`);
+
+    // updateClockFase(days, hours, minutes, seconds);
+    // updateClockFase();
+  }, 1000);
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function updateClockFase({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
