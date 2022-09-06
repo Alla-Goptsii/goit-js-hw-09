6a2b1a2b9c37deb607680ebd25ff2b5a01a206c7
Alla-Goptsii <99249674+Alla-Goptsii@users.noreply.github.com>
2022-09-06 21:53:16 +0000

    Deploying to gh-pages from @ Alla-Goptsii/goit-js-hw-09@862fecab49f38e7a549c9137a6ee4dc31986b447 🚀

--- 01-color-switcher.0f784aaa.js
+++
@@ -1,2 +0,0 @@
-const t={btnStart:document.querySelector("button[data-start]"),btnStop:document.querySelector("button[data-stop]"),body:document.querySelector("body")};let e=null;t.btnStart.addEventListener("click",(function(){let n=!1;if(n)return;e=setInterval((()=>{t.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),n=!0})),t.btnStop.addEventListener("click",(function(){clearInterval(e)}));
-//# sourceMappingURL=01-color-switcher.0f784aaa.js.map
